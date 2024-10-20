a1ef24c62f8796161fed33d226411a979d83e83e
Adina Abdou salam <adina.aabdousalam976@hotmail.com>
2024-10-20 20:37:01 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice_15/Exercice_15.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_15
+{
+    class Exercice_15
+    {
+        static void Main(string[] args)
+        {
+
+                                    // Code_perso
+
+            string phrase = "Bonjour à tous, vous allez bien ?";
+
+            Console.WriteLine(phrase.Replace(' ', '_'));
+            Console.ReadLine();
+
+
+
+                                            // code_correction
+
+            // Saisie de la phrase
+            Console.WriteLine("Entrez une phrase : ");
+            string phrase = Console.ReadLine();
+            // Remplacement des espaces par _
+            string newphrase = phrase.Replace(' ', '_');
+            // Affichage de la nouvelle phrase
+            Console.WriteLine(newphrase);
+            Console.ReadLine();
+
+        }
+    }
+}
